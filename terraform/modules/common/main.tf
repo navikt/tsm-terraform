9d9045ff81b0aeafb876b2add4cedc381cb1cf07
Andreas Sagen Aspaas <danduras@gmail.com>
2025-03-21 09:32:41 +0100

    chore: use variables from google secret manager

--- terraform/modules/common/main.tf
+++ terraform/modules/common/main.tf
@@ -1,6 +1,13 @@
+data "google_secret_manager_secret_version" "sykmelding-bucket" {
+  secret    = "sykmelding-bucket"
+  version   = "latest"
+}
+
+
+
 resource "google_storage_bucket" "sykmelding-xml" {
   location = var.location
-  name     = var.bucket-name
+  name     = data.google_secret_manager_secret_version.sykmelding-bucket.secret_data
 
   uniform_bucket_level_access = true
   labels = {
--- terraform/modules/common/variables.tf
+++ terraform/modules/common/variables.tf
@@ -2,7 +2,3 @@ variable "location" {
   default = "europe-north1"
   type = string
 }
-
-variable "bucket-name" {
-  type = string
-}
