5b52d593ae845f5f001ff9736ba5a248b4d50e0f
Andreas Sagen Aspaas <danduras@gmail.com>
2025-03-21 10:40:48 +0100

    chore: add permission for sykmeldinger-upload

--- terraform/modules/common/main.tf
+++ terraform/modules/common/main.tf
@@ -3,7 +3,10 @@ data "google_secret_manager_secret_version" "sykmelding-bucket" {
   version   = "latest"
 }
 
-
+data "google_secret_manager_secret_version" "tsm-sykmelding-bucket-upload-sa" {
+  secret = "tsm-sykmelding-bucket-upload-sa"
+  version = "latest"
+}
 
 resource "google_storage_bucket" "sykmelding-xml" {
   location = var.location
@@ -13,4 +16,10 @@ resource "google_storage_bucket" "sykmelding-xml" {
   labels = {
     team = "tsm"
   }
+}
+
+resource "google_storage_bucket_iam_member" "sykmelding-upload-member" {
+  bucket = google_storage_bucket.sykmelding-xml.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.google_secret_manager_secret_version.tsm-sykmelding-bucket-upload-sa.secret_data}"
 }
