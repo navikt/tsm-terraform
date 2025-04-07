5c5c4c0c1ee25071d27902602b7817fb17b5ad9a
Helene Arnesen <86235924+helehar@users.noreply.github.com>
2025-04-07 13:26:25 +0200

    chore: add journey sa to google secret manager (#5)

--- terraform/modules/common/main.tf
+++ terraform/modules/common/main.tf
@@ -8,6 +8,11 @@ data "google_secret_manager_secret_version" "tsm-sykmelding-bucket-upload-sa" {
   version = "latest"
 }
 
+data "google_secret_manager_secret_version" "journey-sa" {
+  secret = "journey-sa"
+  version = "latest"
+}
+
 resource "google_storage_bucket" "sykmelding-xml" {
   location = var.location
   name     = data.google_secret_manager_secret_version.sykmelding-bucket.secret_data
@@ -27,4 +32,10 @@ resource "google_storage_bucket_iam_member" "sykmelding-upload-member" {
   bucket = google_storage_bucket.sykmelding-xml.name
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${data.google_secret_manager_secret_version.tsm-sykmelding-bucket-upload-sa.secret_data}"
+}
+
+resource "google_storage_bucket_iam_member" "journey-upload-member" {
+  bucket = google_storage_bucket.sykmelding-xml.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.google_secret_manager_secret_version.journey-sa.secret_data}"
 }
