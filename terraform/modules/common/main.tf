0f3e4889efaebe34645fc21d556ea677df6ced19
Jørn-Are <ja.flaten91@gmail.com>
2026-05-22 13:04:51 +0200

    chore: adding papirsykmelding bucket and granting access to syfosmpapirmottak

--- terraform/modules/common/main.tf
+++ terraform/modules/common/main.tf
@@ -18,9 +18,34 @@ data "google_secret_manager_secret_version" "syfosmmottak-sa" {
   version = "latest"
 }
 
+data "google_secret_manager_secret_version" "papirsykmelding-bucket" {
+  secret  = "papirsykmelding-bucket"
+  version = "latest"
+}
+
+data "google_secret_manager_secret_version" "syfosmpapirmottak-sa" {
+  secret  = "syfosmpapirmottak-sa"
+  version = "latest"
+}
+
 resource "google_storage_bucket" "sykmelding-xml" {
   location = var.location
   name     = data.google_secret_manager_secret_version.sykmelding-bucket.secret_data
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  versioning {
+    enabled = true
+  }
+  labels = {
+    team = "tsm"
+  }
+}
+
+resource "google_storage_bucket" "papirsykmelding-bucket" {
+  location = var.location
+  name     = data.google_secret_manager_secret_version.papirsykmelding-bucket.secret_data
 
   uniform_bucket_level_access = true
   public_access_prevention    = "enforced"
@@ -49,4 +74,10 @@ resource "google_storage_bucket_iam_member" "syfosmmottak-member" {
   bucket = google_storage_bucket.sykmelding-xml.name
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${data.google_secret_manager_secret_version.syfosmmottak-sa.secret_data}"
+}
+
+resource "google_storage_bucket_iam_member" "papirsykmelding-member" {
+  bucket = google_storage_bucket.papirsykmelding-bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.google_secret_manager_secret_version.teamsykmelding.syfosmpapirmottak-sa.secret_data}"
 }
