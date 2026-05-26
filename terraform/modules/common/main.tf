a2c26d65a35f99ced8b029c0ba1146665b8b7de5
Jørn-Are <ja.flaten91@gmail.com>
2026-05-26 10:29:06 +0200

    chore: updated names after creating gcloud secrets
    Co-authored-by: Andreas <danduras@gmail.com>

--- terraform/modules/common/main.tf
+++ terraform/modules/common/main.tf
@@ -18,8 +18,8 @@ data "google_secret_manager_secret_version" "syfosmmottak-sa" {
   version = "latest"
 }
 
-data "google_secret_manager_secret_version" "papirsykmelding-bucket" {
-  secret  = "papirsykmelding-bucket"
+data "google_secret_manager_secret_version" "ocr-bucket" {
+  secret  = "ocr-bucket"
   version = "latest"
 }
 
@@ -43,9 +43,9 @@ resource "google_storage_bucket" "sykmelding-xml" {
   }
 }
 
-resource "google_storage_bucket" "papirsykmelding-bucket" {
+resource "google_storage_bucket" "ocr-bucket" {
   location = var.location
-  name     = data.google_secret_manager_secret_version.papirsykmelding-bucket.secret_data
+  name     = data.google_secret_manager_secret_version.ocr-bucket.secret_data
 
   uniform_bucket_level_access = true
   public_access_prevention    = "enforced"
@@ -77,7 +77,7 @@ resource "google_storage_bucket_iam_member" "syfosmmottak-member" {
 }
 
 resource "google_storage_bucket_iam_member" "papirsykmelding-member" {
-  bucket = google_storage_bucket.papirsykmelding-bucket.name
+  bucket = google_storage_bucket.ocr-bucket.name
   role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:${data.google_secret_manager_secret_version.teamsykmelding.syfosmpapirmottak-sa.secret_data}"
+  member = "serviceAccount:${data.google_secret_manager_secret_version.syfosmpapirmottak-sa.secret_data}"
 }
